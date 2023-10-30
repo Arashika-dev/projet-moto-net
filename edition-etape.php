1930d3c9445455b86d8f67c559a50a26636b0aaf
Laptop-Arashika <morelalessi.florian@gmail.com>
2023-10-30 11:37:52 +0100

    ajout etape edition

---
+++ edition-etape.php
@@ -0,0 +1,18 @@
+<?php 
+require_once __DIR__ ."/layout/header.php";
+
+if(!isset($_GET["brand"]) || !isset($_GET["category"])) {
+    
+}
+?>
+
+<main>
+    <section class="container">
+        <h1 class="mb-5">Étape 2</h1>
+        <form action="">
+
+        </form>
+    </section>
+</main>
+
+<?php require_once __DIR__ ."/layout/footer.php";
--- edition.php
+++ edition.php
@@ -4,16 +4,18 @@ require_once __DIR__ ."/functions/db.php";
 
 $pdo = getConnection();
 
-$brand = $pdo->query("SELECT * FROM brand");
 $type = $pdo->query("SELECT * FROM type");
+$brand = $pdo->query("SELECT * FROM brand");
+$category = $pdo->query("SELECT * FROM category_moto");
+$modelSql = "SELECT * FROM "
 ?>
 
 <main>
     <section class="container">
-        <h1>Créez votre article</h1>
-        <div class="col-lg-3">
-            <form action="edition_publication.php" method="POST" enctype="multipart/form-data">
-                <div class="row g-3">
+        <h1 class="mb-5">Mode édition</h1>
+        
+            <form action="edition_etape.php" method="GET">
+                <div class="col-2 mb-3">
                     <label for="type" class="form-label">Type article</label>
                     <select name="type" id="type" class="form-select" aria-label="Type Article">
                         <?php 
@@ -24,8 +26,34 @@ $type = $pdo->query("SELECT * FROM type");
                         ?>
                     </select>
                 </div>
+                <div class="row g-3 mb-4">
+                    <div class="col-md-3">
+                        <label for="brand" class="form-label">Marque :</label>
+                        <select name="brand" id="brand" class="form-select" aria-label="Marques moto">
+                            <?php 
+                                while($row = $brand->fetch())
+                                { ?>
+                                    <option value="<?php echo $row['brand_id']?>"><?php echo $row['brand_name'] ?></option>
+                            <?php }
+                            ?>
+                        </select>
+                    </div>
+                    <div class="col-md-3">
+                        <label for="category" class="form-label">Catégorie :</label>
+                        <select name="category" id="category" class="form-select" aria-label="Marques moto">
+                            <?php 
+                                while($row = $category->fetch())
+                                { ?>
+                                    <option value="<?php echo $row['category_id']?>"><?php echo $row['category_name'] ?></option>
+                            <?php }
+                            ?>
+                        </select>
+                    </div>
+                </div>
+                <div>
+                    <button class="btn btn-primary" type="submit">Étape suivante</button>
+                </div>
             </form>
-        </div>
     </section>
 </main>
 
